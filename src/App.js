8231ef68f2cfea19de55fd1cf63ee8ec7a3169f0
Astarote <astarote@gmail.com>
2026-04-20 16:50:42 -0300

    Meu primeiro commit React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './avancatechReactJs.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aluno T.I  - AvançaTech - REACT JS
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://l1nq.com/avancatech"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Site AvançaTech
         </a>
       </header>
     </div>
